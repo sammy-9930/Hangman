fd6e118df301f01bd53b9ad430b64dffea6a1709
Samantha <samanthamenezes99@gmail.com>
2023-10-05 20:23:49 +0530

    adding number guessing game

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 from art import end_text, logo
 from hangman import hangman
-
+from number_guessing import number_guessing
 
 def validate_user_choice(user_input):
     if user_input == 0 or user_input == 1:
@@ -34,15 +34,16 @@ def main_menu():
                     input(
                         "Press 1 to play Hangman\n"
                         "Press 2 to play Turtle crossing game\n"
+                        "Press 3 to play the number guessing game \n"
                         "Press 0 to exit\n"
                     )
                 )
                 if game_choice == 1:
-                    print("Welcome to the Hangman game")
                     hangman.run_hangman()
-
                 elif game_choice == 2:
-                    print("Welcome to the turtle crossing game")
+                    print("Welcome to the turtle crossing game.")
+                elif game_choice == 3:
+                    number_guessing.number_guessing_game()
                 else:
                     print(end_text)
                     sys.exit("Glad to see you. \n Warm regards :)")
